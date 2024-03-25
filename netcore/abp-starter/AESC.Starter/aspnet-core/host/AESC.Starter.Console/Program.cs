0353ca2babddcdf093081598d757b80a41da92a8
wang-jie <421004034@qq.com>
2024-03-25 14:06:19 +0800

    增加控制台

---
+++ netcore/abp-starter/AESC.Starter/aspnet-core/host/AESC.Starter.Console/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Volo.Abp;
+
+namespace AESC.Starter.Console
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Async(c => c.File("Logs/logs.txt"))
+                .WriteTo.Async(c => c.Console())
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting console host.");
+
+                var builder = Host.CreateApplicationBuilder(args);
+
+                builder.Logging.ClearProviders().AddSerilog();
+                builder.ConfigureContainer(builder.Services.AddAutofacServiceProviderFactory());
+
+                await builder.Services.AddApplicationAsync<StarterConsoleAppModule>();
+
+                var host = builder.Build();
+
+                await host.InitializeAsync();
+
+                await host.RunAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                if (ex is HostAbortedException)
+                {
+                    throw;
+                }
+
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+                return 1;
+            }
+            finally
+            {
+                await Log.CloseAndFlushAsync();
+            }
+        }
+    }
+}
---
+++ netcore/abp-starter/AESC.Starter/aspnet-core/host/AESC.Starter.Console/StarterConsoleAppModule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace AESC.Starter.Console
+{
+    [DependsOn(
+        typeof(AbpAutofacModule)
+    )]
+    public class StarterConsoleAppModule : AbpModule
+    {
+        public override Task OnApplicationInitializationAsync(ApplicationInitializationContext context)
+        {
+            var logger = context.ServiceProvider.GetRequiredService<ILogger<StarterConsoleAppModule>>();
+            var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
+            var hostEnvironment = context.ServiceProvider.GetRequiredService<IHostEnvironment>();
+
+            logger.LogInformation($"EnvironmentName => {hostEnvironment.EnvironmentName}");
+            logger.LogInformation($"Connection => {configuration.GetConnectionString("Default")}");
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
