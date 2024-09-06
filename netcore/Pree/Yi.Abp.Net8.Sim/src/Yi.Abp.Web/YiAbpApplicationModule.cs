1d6e7a311650c928621fcc5afa1ce03129fcdfc2
wang-jie <421004034@qq.com>
2024-09-06 09:31:37 +0800

    do merge

---
+++ netcore/Pree/Yi.Abp.Net8.Sim/src/Yi.Abp.Web/YiAbpApplicationModule.cs
@@ -0,0 +1,31 @@
+﻿using Volo.Abp.SettingManagement;
+using Yi.Abp.Application.Contracts;
+using Yi.Abp.Domain;
+using Yi.Framework.Bbs.Application;
+using Yi.Framework.ChatHub.Application;
+using Yi.Framework.CodeGen.Application;
+using Yi.Framework.Ddd.Application;
+using Yi.Framework.Rbac.Application;
+using Yi.Framework.SettingManagement.Application;
+using Yi.Framework.TenantManagement.Application;
+
+namespace Yi.Abp.Application
+{
+    [DependsOn(
+        typeof(YiAbpApplicationContractsModule),
+        typeof(YiAbpDomainModule),
+
+
+        typeof(YiFrameworkRbacApplicationModule),
+         typeof(YiFrameworkBbsApplicationModule),
+         typeof(YiFrameworkChatHubApplicationModule),
+        typeof(YiFrameworkTenantManagementApplicationModule),
+        typeof(YiFrameworkCodeGenApplicationModule),
+        typeof (YiFrameworkSettingManagementApplicationModule),
+
+        typeof(YiFrameworkDddApplicationModule)
+        )]
+    public class YiAbpApplicationModule : AbpModule
+    {
+    }
+}
---
+++ netcore/Pree/Yi.Abp.Net8.Sim/src/Yi.Abp.Web/YiAbpDomainSharedModule.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Domain;
+using Volo.Abp.SettingManagement;
+using Yi.Framework.AuditLogging.Domain.Shared;
+using Yi.Framework.Bbs.Domain.Shared;
+using Yi.Framework.ChatHub.Domain.Shared;
+using Yi.Framework.Rbac.Domain.Shared;
+
+namespace Yi.Abp.Domain.Shared
+{
+    [DependsOn(
+        typeof(YiFrameworkRbacDomainSharedModule),
+        typeof(YiFrameworkBbsDomainSharedModule),
+          typeof(YiFrameworkChatHubDomainSharedModule),
+        typeof(YiFrameworkAuditLoggingDomainSharedModule),
+
+        typeof(AbpSettingManagementDomainSharedModule),
+        typeof(AbpDddDomainSharedModule))]
+    public class YiAbpDomainSharedModule : AbpModule
+    {
+
+    }
+}
