d2f105d1f9a059d0e610ab4a01b86edebf45a03b
wang-jie <421004034@qq.com>
2024-09-11 10:38:10 +0800

    1

---
+++ netcore/Pree/Yi.Abp.Net8.Sim/src/Yi.Web/HelloJob.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using Volo.Abp.BackgroundWorkers.Quartz;
+using Yi.Framework.SqlSugarCore;
+using Yi.Infra.Rbac.Entities;
+
+namespace Yi.Web;
+
+public class HelloJob : QuartzBackgroundWorkerBase
+{
+    public HelloJob()
+    {
+        JobDetail = JobBuilder.Create<HelloJob>().WithIdentity(nameof(HelloJob)).Build();
+
+        // Trigger = TriggerBuilder.Create().WithIdentity(nameof(HelloJob)).StartNow()
+        //     .WithSimpleSchedule(x => x.WithIntervalInSeconds(1000 * 60).RepeatForever())
+        //     .Build();
+        
+        Trigger = TriggerBuilder.Create().WithIdentity(nameof(HelloJob)).WithCronSchedule("0 0 * * * ? ").Build();
+    }
+
+    public override async Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine("你好，世界");
+    }
+}
