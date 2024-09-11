f9b53958e087c04dc475f189d1259ec7202c72cd
wang-jie <421004034@qq.com>
2024-09-11 12:50:38 +0800

    online

--- netcore/Pree/Yi.Abp.Net8.Sim/framework/Yi.Infra/Monitor/OnlineController.cs
+++ netcore/Pree/Yi.Abp.Net8.Sim/framework/Yi.Infra/Monitor/OnlineController.cs
@@ -3,54 +3,58 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using Volo.Abp.AspNetCore.Mvc;
 using Yi.Infra.Rbac.IServices;
 using Yi.Infra.Rbac.Model;
 using Yi.Infra.Rbac.SignalRHubs;
 
 namespace Yi.Infra.Monitor;
 
-public class OnlineService : ApplicationService, IOnlineService
+[ApiController]
+[Route("api/app/online")]
+public class OnlineController : AbpController
 {
+    private readonly ILogger<OnlineController> _logger;
     private readonly IHubContext<OnlineHub> _hub;
-    private ILogger<OnlineService> _logger;
 
-    public OnlineService(ILogger<OnlineService> logger, IHubContext<OnlineHub> hub)
+    public OnlineController(ILogger<OnlineController> logger, IHubContext<OnlineHub> hub)
     {
         _logger = logger;
         _hub = hub;
     }
 
-    /// <summary>
-    ///     动态条件获取当前在线用户
-    /// </summary>
-    /// <param name="online"></param>
-    /// <returns></returns>
+    [HttpGet]
     public Task<PagedResultDto<OnlineUserModel>> GetListAsync([FromQuery] OnlineUserModel online)
     {
         var data = OnlineHub.clientUsers;
         var dataWhere = data.AsEnumerable();
 
-        if (!string.IsNullOrEmpty(online.Ipaddr)) dataWhere = dataWhere.Where(u => u.Ipaddr!.Contains(online.Ipaddr));
+        if (!string.IsNullOrEmpty(online.Ipaddr))
+        {
+            dataWhere = dataWhere.Where(u => u.Ipaddr!.Contains(online.Ipaddr));
+        }
+
         if (!string.IsNullOrEmpty(online.UserName))
+        {
             dataWhere = dataWhere.Where(u => u.UserName!.Contains(online.UserName));
-        return Task.FromResult(new PagedResultDto<OnlineUserModel>
-            { TotalCount = data.Count, Items = dataWhere.ToList() });
+        }
+
+        return Task.FromResult(new PagedResultDto<OnlineUserModel> { TotalCount = data.Count, Items = dataWhere.ToList() });
     }
 
 
     /// <summary>
     ///     强制退出用户
     /// </summary>
-    /// <param name="connnectionId"></param>
+    /// <param name="connectionId"></param>
     /// <returns></returns>
-    [HttpDelete]
-    [Route("online/{connnectionId}")]
-    public async Task<bool> ForceOut(string connnectionId)
+    [HttpDelete("{connectionId}")]
+    public async Task<bool> ForceOut([FromRoute] string connectionId)
     {
-        if (OnlineHub.clientUsers.Exists(u => u.ConnnectionId == connnectionId))
+        if (OnlineHub.clientUsers.Exists(u => u.ConnnectionId == connectionId))
         {
             //前端接受到这个事件后，触发前端自动退出
-            await _hub.Clients.Client(connnectionId).SendAsync("forceOut", "你已被强制退出！");
+            await _hub.Clients.Client(connectionId).SendAsync("forceOut", "你已被强制退出！");
             return true;
         }
 
--- netcore/Pree/Yi.Abp.Net8.Sim/framework/Yi.Infra/Rbac/Services/IOnlineService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Dtos;
-using Yi.Infra.Rbac.Model;
-
-namespace Yi.Infra.Rbac.IServices;
-
-public interface IOnlineService
-{
-    Task<PagedResultDto<OnlineUserModel>> GetListAsync(OnlineUserModel online);
-}
