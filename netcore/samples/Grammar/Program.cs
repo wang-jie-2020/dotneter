34e5b6fcafd49811a2d9478f65d5275a3e066529
wang-jie <421004034@qq.com>
2025-04-09 09:05:39 +0800

    2

---
+++ netcore/samples/Grammar/Program.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+
+namespace Grammar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Run(Type type)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入命令：0; 退出程序，功能命令：1 - n");
+                string input = Console.ReadLine() ?? string.Empty;
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                if (input == "0")
+                {
+                    break;
+                }
+
+                if (type != null)
+                {
+                    object? o = Activator.CreateInstance(type);
+
+                    try
+                    {
+                        type.InvokeMember("Method" + input,
+                            BindingFlags.Static | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.NonPublic |
+                            BindingFlags.InvokeMethod,
+                            null, o,
+                            new object[] { });
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+    }
+}
