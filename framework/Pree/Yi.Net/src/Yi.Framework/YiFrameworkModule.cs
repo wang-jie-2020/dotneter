33619ea9600bf886c821e241a4ac035881c1a807
wang-jie <421004034@qq.com>
2025-06-17 13:13:03 +0800

    1

--- framework/Pree/Yi.Net/src/Yi.Framework/YiFrameworkModule.cs
+++ framework/Pree/Yi.Net/src/Yi.Framework/YiFrameworkModule.cs
@@ -1,11 +1,21 @@
-﻿using Yi.AspNetCore;
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.Uow;
 
 namespace Yi.Framework;
 
 [DependsOn(
-    typeof(YiAspNetCoreModule)
+    typeof(AbpAutofacModule),
+    typeof(AbpUnitOfWorkModule)
 )]
 public class YiFrameworkModule : AbpModule
 {
-    
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        
+    }
 }
