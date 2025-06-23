ec61cbb5999c40f1be63ce8af281f9be1f0dc17b
wang-jie <421004034@qq.com>
2025-06-23 11:44:22 +0800

    1

--- framework/Pree/Yi.Net/src/Yi.Admin/AdminModule.cs
+++ framework/Pree/Yi.Net/src/Yi.Admin/AdminModule.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json.Converters;
 using Yi.AspNetCore;
 using Yi.AspNetCore.Extensions.Builder;
 using Yi.AspNetCore.Extensions.DependencyInjection;
-using Yi.Framework.Auditing;
+using Yi.Framework.Extensions.Builder;
 using Yi.Framework.Permissions;
 using Yi.Framework.SqlSugarCore;
 using Yi.Framework.Utils;
---
+++ framework/Pree/Yi.Net/src/Yi.Framework/Extensions/Builder/AuditingApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Yi.Framework.Auditing;
+
+namespace Yi.Framework.Extensions.Builder
+{
+    public static class AuditingApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseAuditing(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<AuditingMiddleware>();
+        }
+    }
+}
