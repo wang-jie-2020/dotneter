3473700c85f2420ca494b8b5bd3eb74017c3a54f
wang-jie <421004034@qq.com>
2025-06-24 15:09:14 +0800

    1

--- framework/Pree/Yi.Net/src/Yi.Admin/AdminModule.cs
+++ framework/Pree/Yi.Net/src/Yi.Admin/AdminModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection;
 using System.Text;
 using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -11,6 +12,7 @@ using Newtonsoft.Json.Converters;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using Yi.AspNetCore;
+using Yi.AspNetCore.Data.Seeding;
 using Yi.AspNetCore.Extensions.Builder;
 using Yi.AspNetCore.Extensions.DependencyInjection;
 using Yi.Framework.Extensions.Builder;
@@ -197,6 +199,50 @@ public class AdminModule : AbpModule
         //}
 
         return Task.CompletedTask;
+    }
+
+    public override async Task OnPreApplicationInitializationAsync(ApplicationInitializationContext context)
+    {
+        var service = context.ServiceProvider;
+        var options = service.GetRequiredService<IOptions<DbConnOptions>>().Value;
+
+        // var sb = new StringBuilder();
+        // sb.AppendLine();
+        // sb.AppendLine("==========Yi-SQL配置:==========");
+        // sb.AppendLine($"数据库连接字符串：{options.Url}");
+        // sb.AppendLine($"数据库类型：{options.DbType.ToString()}");
+        // sb.AppendLine($"是否开启种子数据：{options.EnabledDbSeed}");
+        // sb.AppendLine($"是否开启CodeFirst：{options.EnabledCodeFirst}");
+        // sb.AppendLine($"是否开启Saas多租户：{options.EnabledSaasMultiTenancy}");
+        // sb.AppendLine("===============================");
+        // var logger = service.GetRequiredService<ILogger<YiFrameworkSqlSugarCoreModule>>();
+        // logger.LogInformation(sb.ToString());
+
+        if (options.EnabledCodeFirst) CodeFirst(service);
+        if (options.EnabledDbSeed) await DataSeedAsync(service);
+    }
+
+    private void CodeFirst(IServiceProvider service)
+    {
+        var moduleContainer = service.GetRequiredService<IModuleContainer>();
+        var db = service.GetRequiredService<ISqlSugarDbContext>().SqlSugarClient;
+
+        //尝试创建数据库
+        db.DbMaintenance.CreateDatabase();
+
+        var types = new List<Type>();
+        foreach (var module in moduleContainer.Modules)
+            types.AddRange(module.Assembly.GetTypes()
+                .Where(x => x.GetCustomAttribute<IgnoreCodeFirstAttribute>() == null)
+                .Where(x => x.GetCustomAttribute<SugarTable>() != null)
+                .Where(x => x.GetCustomAttribute<SplitTableAttribute>() is null));
+        if (types.Count > 0) db.CopyNew().CodeFirst.InitTables(types.ToArray());
+    }
+
+    private async Task DataSeedAsync(IServiceProvider service)
+    {
+        var dataSeeder = service.GetRequiredService<IDataSeeder>();
+        await dataSeeder.SeedAsync();
     }
 
     public override Task OnApplicationInitializationAsync(ApplicationInitializationContext context)
--- framework/Pree/Yi.Net/src/Yi.Framework/YiFrameworkModule.cs
+++ framework/Pree/Yi.Net/src/Yi.Framework/YiFrameworkModule.cs
@@ -64,48 +64,4 @@ public class YiFrameworkModule : AbpModule
         context.Services.AddSingleton<IMiniProfilerDiagnosticListener, SqlSugarDiagnosticListener>();
         context.Services.AddSingleton<ITracingDiagnosticProcessor, SqlSugarTracingDiagnosticProcessor>();
     }
-
-    public override async Task OnPreApplicationInitializationAsync(ApplicationInitializationContext context)
-    {
-        var service = context.ServiceProvider;
-        var options = service.GetRequiredService<IOptions<DbConnOptions>>().Value;
-
-        // var sb = new StringBuilder();
-        // sb.AppendLine();
-        // sb.AppendLine("==========Yi-SQL配置:==========");
-        // sb.AppendLine($"数据库连接字符串：{options.Url}");
-        // sb.AppendLine($"数据库类型：{options.DbType.ToString()}");
-        // sb.AppendLine($"是否开启种子数据：{options.EnabledDbSeed}");
-        // sb.AppendLine($"是否开启CodeFirst：{options.EnabledCodeFirst}");
-        // sb.AppendLine($"是否开启Saas多租户：{options.EnabledSaasMultiTenancy}");
-        // sb.AppendLine("===============================");
-        // var logger = service.GetRequiredService<ILogger<YiFrameworkSqlSugarCoreModule>>();
-        // logger.LogInformation(sb.ToString());
-
-        if (options.EnabledCodeFirst) CodeFirst(service);
-        if (options.EnabledDbSeed) await DataSeedAsync(service);
-    }
-
-    private void CodeFirst(IServiceProvider service)
-    {
-        var moduleContainer = service.GetRequiredService<IModuleContainer>();
-        var db = service.GetRequiredService<ISqlSugarDbContext>().SqlSugarClient;
-
-        //尝试创建数据库
-        db.DbMaintenance.CreateDatabase();
-
-        var types = new List<Type>();
-        foreach (var module in moduleContainer.Modules)
-            types.AddRange(module.Assembly.GetTypes()
-                .Where(x => x.GetCustomAttribute<IgnoreCodeFirstAttribute>() == null)
-                .Where(x => x.GetCustomAttribute<SugarTable>() != null)
-                .Where(x => x.GetCustomAttribute<SplitTableAttribute>() is null));
-        if (types.Count > 0) db.CopyNew().CodeFirst.InitTables(types.ToArray());
-    }
-
-    private async Task DataSeedAsync(IServiceProvider service)
-    {
-        var dataSeeder = service.GetRequiredService<IDataSeeder>();
-        await dataSeeder.SeedAsync();
-    }
 }
