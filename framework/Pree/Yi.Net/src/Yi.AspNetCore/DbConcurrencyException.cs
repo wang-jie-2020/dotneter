c7c2998780f39a2443079d16808f85fb58ce2808
wang-jie <421004034@qq.com>
2025-07-03 10:51:11 +0800

    两个exception

---
+++ framework/Pree/Yi.Net/src/Yi.AspNetCore/DbConcurrencyException.cs
@@ -0,0 +1,21 @@
+﻿namespace Yi.AspNetCore;
+
+public class DbConcurrencyException : Exception
+{
+    public DbConcurrencyException()
+    {
+
+    }
+    
+    public DbConcurrencyException(string message)
+        : base(message)
+    {
+
+    }
+    
+    public DbConcurrencyException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+
+    }
+}
