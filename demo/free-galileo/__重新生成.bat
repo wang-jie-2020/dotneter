d5ecb3ac7ce080e5143bba3555fded565f22dc81
wang-jie <421004034@qq.com>
2026-04-09 11:13:32 +0800

    1

--- demo/free-galileo/__重新生成.bat
+++ demo/free-galileo/__重新生成.bat
@@ -1,3 +1,3 @@
-C:\Users\jie.wang21\.dotnet\tools\FreeSql.Generator.exe -Razor "__razor.cshtml.txt" -NameOptions 1,1,0,1 -NameSpace Admin.NET.Core -Match SYS_INVENTORYREPLENISHMENT_HISTORY -DB  "Oracle,Data Source=10.202.34.12:1521/GalileoT;User ID=galileo;Password=Dtg@rj1377gh"
+C:\Users\jie.wang21\.dotnet\tools\FreeSql.Generator.exe -Razor "__razor.cshtml.txt" -NameOptions 0,1,0,1 -NameSpace Admin.NET.Core -Match GALILEO_EQP_ALARM_RECORD -DB  "Oracle,Data Source=10.205.161.11:8081/Galileo;User ID=galileo;Password=Dtg@rj1377gh"
 
 pause
